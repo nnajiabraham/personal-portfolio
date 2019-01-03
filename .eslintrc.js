102fa5d733853671380a7bd45cd6ed1e33adceff
Abraham Nnaji <nnajiabraham@gmail.com>
2019-01-03 13:08:42 -0600

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['airbnb', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/require-default-props': 'off',
+    'linebreak-style': ['error', 'windows'],
+  },
+  parser: 'babel-eslint',
+};
